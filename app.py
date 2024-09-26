93267267ec14ce4c879d13cc02e1fb69b79deb57
septo198 <matteopaterlini198@gmail.com>
2024-09-26 12:10:15 +0200

    Dark mode

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify, send_file, make_response
 from main import login, get_followers, get_followings, determine_nonfollowers
 import requests
 from io import BytesIO
@@ -15,17 +15,23 @@ def login_route():
     username = data['username']
     password = data['password']
     message = login(username, password)
-    return jsonify(message=message)
+    response = make_response(jsonify(message=message))
+    response.headers['Cache-Control'] = 'no-store'
+    return response
 
 @app.route('/get_followers', methods=['POST'])
 def get_followers_route():
     followers_dict = get_followers()
-    return jsonify(followers=followers_dict)
+    response = make_response(jsonify(followers=followers_dict))
+    response.headers['Cache-Control'] = 'no-store'
+    return response
 
 @app.route('/get_followings', methods=['POST'])
 def get_followings_route():
     followings_dict = get_followings()
-    return jsonify(followings=followings_dict)
+    response = make_response(jsonify(followings=followings_dict))
+    response.headers['Cache-Control'] = 'no-store'
+    return response
 
 @app.route('/determine_nonfollowers', methods=['POST'])
 def determine_nonfollowers_route():
@@ -33,7 +39,9 @@ def determine_nonfollowers_route():
     followers_dict = data['followers']
     followings_dict = data['followings']
     nonfollowers = determine_nonfollowers(followers_dict, followings_dict)
-    return jsonify(nonfollowers=nonfollowers)
+    response = make_response(jsonify(nonfollowers=nonfollowers))
+    response.headers['Cache-Control'] = 'no-store'
+    return response
 
 @app.route('/proxy_image')
 def proxy_image():
